2efba976179ef6332ba2690dc360a368e152a824
kpeck71 <kpeck71@gmail.com>
2017-12-26 19:38:05 -0500

    fixed database connection and added Rack::MethodOverride

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ ENV['SINATRA_ENV'] ||= "development"
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
-require_relative "../app/controllers/application_controller.rb"
+require_all 'app'
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].sort.each {|f| require f}
