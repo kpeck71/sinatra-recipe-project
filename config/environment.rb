38ca8b6b36696a623eff34dbd1d12de4186b1cc7
kpeck71 <kpeck71@gmail.com>
2017-12-27 21:23:43 -0500

    trying to fix db so can use rake db:reset

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,10 @@
-require 'bundler/setup'
-Bundler.require
-
 ENV['SINATRA_ENV'] ||= "development"
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 
 require_all 'app'
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].sort.each {|f| require f}
